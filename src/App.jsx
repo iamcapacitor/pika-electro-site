127c49d8d12e955f7fb8b2ad7875508ac72343e9
Capacitor <rohitkhengare172@gmail.com>
2026-01-02 17:08:02 +0530

    make Navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -49,13 +49,13 @@ function App() {
   return (
     <>
       <div>Landingpage will be here</div>
-      <section className='servicesection bg-[var(--servicebg)] text-[var(--servicetext)] p-2 py-10 grid place-items-center'>
+      <section className='servicesection bg-(--servicebg) text-(--servicetext) p-2 py-10 grid place-items-center'>
         <h2 className='text-3xl font-bold'>Our services</h2>
         <div className='w-20 min-w-15 m-2 mb-4 border-4 border-amber-300 rounded-full'></div>
         <div className="servicesbox flex flex-wrap justify-center items-center  md:grid  md:grid-cols-3 gap-4 sm:place-items-center w-full md:w-fit">
           {services.map((s, i) => {
             return (
-              <div key={i} className="service1 bg-[var(--scolor)] p-2 py-5 rounded-md grid place-items-center gap-4 text-md font-semibold w-50 md:w-60 min-w-fit max-w-sm hover:scale-[1.02] hover:shadow shadow-amber-200 ">
+              <div key={i} className="service1 bg-(--scolor) p-2 py-5 rounded-md grid place-items-center gap-4 text-md font-semibold w-50 md:w-60 min-w-fit max-w-sm hover:scale-[1.02] hover:shadow shadow-amber-200 ">
                 <div className="icon active:scale-105"><img className='w-7 scale-175' src={s.imglink} alt={s.servicetext} /></div>
                 <div className="servicetext">{s.servicetext}</div>
               </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,13 @@
-import React,{ useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
+import { Palette, Menu} from 'lucide-react';
 
 
 const Navigation = () => {
     console.log("Navigation rerendering")
     const [modeimage, setModeimage] = useState("lightmodeimage.svg")
     const [usertheme, setUsertheme] = useState("dark");
+    const [menu, setMenu] = useState(true);
 
     const changestates = () => {
         if (usertheme === "light") {
@@ -31,19 +33,31 @@ const Navigation = () => {
     }, [])
 
     return (
-        <nav className="card2 bg-[var(--bg2)] p-2 px-3 rounded-md flex flex-row justify-start items-center gap-1.5">
-            <button className='normalbtn2 text-white p-1 px-2 bg-[#343340] rounded-md active:brightness-75 ' onClick={changestates} >
-                <div>
-                    <img src={`/${modeimage}`} className="logo react" alt="theme change logo" />
-                </div>
-            </button>
+        <nav className={`card2 bg-(--bg2) p-3 xl:mx-auto m-3 sticky top-3 max-w-6xl text-(--text)  rounded-xl flex flex-col sm:flex-row justify-between sm:items-center gap-1.5 transition-all overflow-hidden ${menu?"h-15":"h-30"} `}>
+            <div className='flex items-center gap-2'>
+                <div className='p-2 bg-linear-60 from-blue-200 to-sky-500 rounded-lg shadow-sm shadow-white '><Palette className='text-white' /></div>
+                <div className='font-bold text-lg'>Pika-Electro</div>
+            </div>
+
             {/* <div>
                 <p>Theme toggle btn</p>
                 <p>{modeimage} , {usertheme}</p>
-            </div> */}
-            <div className='flex gap-1 place-items-center'>
-                <NavLink to="/page2" >page2</NavLink>
-                <NavLink to="/designpage" >designpage</NavLink>
+                </div> */}
+
+            <div className='flex gap-10 justify-center py-3 ' >
+                <div className='flex gap-1 place-items-center'>
+                    <NavLink to="/page2" className={"bg-(--btnbg) p-1 px-2 rounded-md flex "} >page2</NavLink>
+                    <NavLink to="/designpage" className={"bg-(--btnbg) p-1 px-2 rounded-md "} >design</NavLink>
+                </div>
+                <button className='normalbtn2 text-white p-1 px-2 bg-[#343340] rounded-md active:brightness-75 ' onClick={changestates} >
+                    <div>
+                        <img src={`/${modeimage}`} className="logo react" alt="theme change logo" />
+                    </div>
+                </button>
+            </div>
+
+            <div className='absolute top-4.5 right-3 sm:hidden ' onClick={()=>{setMenu(!menu)}} >
+                <Menu />
             </div>
         </nav>
     )
